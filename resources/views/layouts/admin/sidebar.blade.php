1ca80646cec1703101111042e7fcc451e74dd373
Aldrin-DP <aldrinpelayo.adp@gmail.com>
2025-11-29 14:29:06 +0800

    modify sidebar

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -36,9 +36,6 @@
                 @csrf
                 <button  class="block w-full text-center border border-red-400 text-red-400 font-semibold px-4 py-2 rounded hover:bg-red-200 transition cursor-pointer ">Logout</button>
             </form>
-            
-           
         </div>
-      
     </div>
 </section>
